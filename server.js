0e88838e80ffb83c5dedfa44081c752cab3603f2
Ibrahim Al-Obaide <micoadult2018@gmail.com>
2026-06-04 16:29:16 +0300

    Add CORS middleware to server for improved API access

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204)
+  }
+  next()
+})
 
 async function loadDbFile(filename) {
   const filePath = path.join(__dirname, 'db', filename)
